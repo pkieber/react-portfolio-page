8b52d979734fab15d53d42cc8b2a2a0cf3e24768
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-11 21:13:25 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { ChakraProvider } from "@chakra-ui/react";
 import Header from "./components/Header";
 import './App.css';
 import LandingSection from "./components/LandingSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <ChakraProvider>
         <Header />
         <LandingSection />
+        <Footer />
     </ChakraProvider>
   );
 }
