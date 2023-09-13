a298a576bc2d87bad06cab95af540a925286f65f
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-13 15:41:01 +0200

    bug fixes contact form + clean code

--- src/components/ContactMeSection.js
+++ src/components/ContactMeSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { useFormik } from "formik";
 import {
     Box,
@@ -17,126 +17,105 @@ import FullScreenSection from "./FullScreenSection";
 import useSubmit from "../hooks/useSubmit";
 import { useAlertContext } from "../context/alertContext";
 
-const ContactMeSection = () => {
-    const { submit } = useSubmit();
-    const { onOpen } = useAlertContext();
-    const [isLoading, setIsLoading] = useState(false);
 
-
-    const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            email: '',
-            type: 'hireMe',
-            comment: '',
-        },
-
-        onSubmit: async (values) => {
-            setIsLoading(true);
-
-            const response = await submit(values);
-
-            if (response && response.type === "success") {
-                const message = `Thanks for your submission ${values.firstName}, we will get back to you`;
-                onOpen(response.type, message);
-                formik.resetForm();
-            } else {
-                const errorMessage =
-                    response && response.message ? response.message : "Something went wrong, please try again later!";
-                onOpen("error", errorMessage);
-            }
-
-            setIsLoading(false);
-        },
-
-        validationSchema: Yup.object({
-            firstName: Yup.string().required('Name is required'),
-            email: Yup.string().email('Invalid email address').required('Email is required'),
-            comment: Yup.string()
-                .min(25, "Must be 25 characters at minimum")
-                .required('Message is required'),
-        }),
-    });
-
-    return (
-        <FullScreenSection
-            isDarkBackground
-            backgroundColor="#512DA8"
-            py={16}
-            spacing={8}
-        >
-            <VStack w="1024px" p={32} alignItems="flex-start">
-                <Heading as="h1" id="contactme-section">
-                    Contact me
-                </Heading>
-                <Box p={6} rounded="md" w="100%">
-                    <form onSubmit={formik.handleSubmit}>
-                        <VStack spacing={4}>
-                            <FormControl isInvalid={formik.touched.firstName && formik.errors.firstName}>
-                                <FormLabel htmlFor="firstName">Name</FormLabel>
-                                <Input
-                                    id="firstName"
-                                    name="firstName"
-                                    value={formik.values.firstName}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                />
-                                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
-                            </FormControl>
-                            <FormControl isInvalid={formik.touched.email && formik.errors.email}>
-                                <FormLabel htmlFor="email">Email Address</FormLabel>
-                                <Input
-                                    id="email"
-                                    name="email"
-                                    type="email"
-                                    value={formik.values.email}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                />
-                                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                                <Select
-                                    id="type"
-                                    name="type"
-                                    value={formik.values.type}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                >
-                                    <option value="hireMe">Freelance project proposal</option>
-                                    <option value="openSource">
-                                        Open source consultancy session
-                                    </option>
-                                    <option value="other">Other</option>
-                                </Select>
-                            </FormControl>
-                            <FormControl isInvalid={formik.touched.comment && formik.errors.comment}>
-                                <FormLabel htmlFor="comment">Your message</FormLabel>
-                                <Textarea
-                                    id="comment"
-                                    name="comment"
-                                    height={250}
-                                    value={formik.values.comment}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                />
-                                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
-                            </FormControl>
-                            <Button
-                                type="submit"
-                                colorScheme="purple"
-                                width="full"
-                                isLoading={isLoading}
-                            >
-                                {isLoading ? "Submitting..." : "Submit"}
-                            </Button>
-                        </VStack>
-                    </form>
-                </Box>
-            </VStack>
-        </FullScreenSection>
-    );
-};
-
+/**
+ * Form implementation using formik and yup for validation.
+ * Includes FromControl and FormErrorMessage.
+ */
+const ContactMeSection = () => { 
+    const {isLoading, response, submit} = useSubmit(); 
+    const { onOpen } = useAlertContext(); 
+    
+    const formik = useFormik({ 
+        initialValues: { 
+            firstName: "", 
+            email: "", 
+            type: "hireMe", 
+            comment: "", 
+        }, 
+        onSubmit: (values) => { 
+            submit('https://john.com/contactme', values); 
+        }, 
+        validationSchema: Yup.object({ 
+            firstName: Yup.string().required("Required"), 
+            email: Yup.string().email("Invalid email address").required("Required"), 
+            comment: Yup.string() 
+                .min(25, "Must be at least 25 characters") 
+                .required("Required"), 
+        }), 
+    }); 
+    
+    useEffect(() => { 
+        if (response) { 
+            onOpen(response.type, response.message); 
+            if (response.type === 'success') { 
+                formik.resetForm(); 
+            } 
+        } 
+    }, [response]);
+    
+    return ( 
+        <FullScreenSection 
+            isDarkBackground 
+            backgroundColor="#512DA8" 
+            py={16} 
+            spacing={8} 
+        > 
+            <VStack w="1024px" p={32} alignItems="flex-start"> 
+                <Heading as="h1" id="contactme-section"> 
+                    Contact me 
+                </Heading> 
+                <Box p={6} rounded="md" w="100%"> 
+                    <form onSubmit={formik.handleSubmit}> 
+                        <VStack spacing={4}> 
+                            <FormControl isInvalid={!!formik.errors.firstName && formik.touched.firstName}> 
+                                <FormLabel htmlFor="firstName">Name</FormLabel> 
+                                <Input 
+                                    id="firstName" 
+                                    name="firstName" 
+                                    {...formik.getFieldProps("firstName")} 
+                                /> 
+                                <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage> 
+                            </FormControl> 
+                                <FormControl isInvalid={!!formik.errors.email && formik.touched.email}> 
+                                <FormLabel htmlFor="email">Email Address</FormLabel> 
+                                <Input 
+                                    id="email" 
+                                    name="email" 
+                                    type="email" 
+                                    {...formik.getFieldProps("email")} 
+                                /> 
+                                <FormErrorMessage>{formik.errors.email}</FormErrorMessage> 
+                            </FormControl> 
+                            <FormControl> 
+                                <FormLabel htmlFor="type">Type of enquiry</FormLabel> 
+                                <Select id="type" name="type" {...formik.getFieldProps("type")}> 
+                                    <option value="hireMe">Freelance project proposal</option> 
+                                    <option value="openSource"> 
+                                        Open source consultancy session 
+                                    </option> 
+                                    <option value="other">Other</option> 
+                                </Select> 
+                            </FormControl> 
+                            <FormControl isInvalid={!!formik.errors.comment && formik.touched.comment}> 
+                                <FormLabel htmlFor="comment">Your message</FormLabel> 
+                                <Textarea 
+                                    id="comment" 
+                                    name="comment" 
+                                    height={250} 
+                                    {...formik.getFieldProps("comment")} 
+                                /> 
+                                <FormErrorMessage>{formik.errors.comment}</FormErrorMessage> 
+                            </FormControl> 
+                            <Button type="submit" colorScheme="purple" width="full" isLoading={isLoading}> 
+                                Submit 
+                            </Button> 
+                        </VStack> 
+                    </form> 
+                </Box> 
+            </VStack> 
+        </FullScreenSection> 
+    ); 
+}; 
+    
 export default ContactMeSection;
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -7,25 +7,25 @@ const projects = [
     {
         title: "React Space",
         description:
-        "Handy tool belt to create amazing AR components in a React app, with redux integration via middleware️",
+            "Handy tool belt to create amazing AR components in a React app, with redux integration via middleware️",
         getImageSrc: () => require("../images/photo1.jpg"),
     },
     {
         title: "React Infinite Scroll",
         description:
-        "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land 🔥️",
+            "A scrollable bottom sheet with virtualisation support, native animations at 60 FPS and fully implemented in JS land 🔥️",
         getImageSrc: () => require("../images/photo2.jpg"),
     },
     {
         title: "Photo Gallery",
         description:
-        "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
+            "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
         getImageSrc: () => require("../images/photo3.jpg"),
     },
     {
         title: "Event planner",
         description:
-        "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
+            "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
         getImageSrc: () => require("../images/photo4.jpg"),
     },
 ];
@@ -48,12 +48,12 @@ const ProjectsSection = () => {
             gridGap={8}
         >
             {projects.map((project) => (
-            <Card
-                key={project.title}
-                title={project.title}
-                description={project.description}
-                imageSrc={project.getImageSrc()}
-            />
+                <Card
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    imageSrc={project.getImageSrc()}
+                />
             ))}
         </Box>
         </FullScreenSection>
